d939707e39aca3a78fac4e41a1e2f753b6a22aa6
BaigHasan <baig.hasan@outlook.com>
2023-09-14 14:49:57 -0400

    Finished the frame and logic

--- src/changeExchange.java
+++ src/changeExchange.java
@@ -10,6 +10,21 @@ public class changeExchange extends JFrame {
     private JTextField txtCurrencyInput;
     private JLabel lblAmount;
     private JButton btnCalculate;
+    private JLabel lblAmountInput;
+    private JLabel lblAmountInputDisplay;
+    private JPanel amountPanel;
+    private JLabel lblQuarters;
+    private JLabel lblNickels;
+    private JLabel lblNickelsOutput;
+    private JLabel lblQuartersOutput;
+    private JLabel lblLoonies;
+    private JLabel lblLooniesOutput;
+    private JLabel lblDimes;
+    private JLabel lblDimesOutput;
+    private JLabel lblPennies;
+    private JLabel lblPenniesOutput;
+    private JLabel lblToonies;
+    private JLabel lblTooniesOutput;
 
     public changeExchange (){
         setContentPane(changeExchangePanel);
@@ -22,6 +37,32 @@ public class changeExchange extends JFrame {
             @Override
             public void actionPerformed(ActionEvent e) {
                 double currency = Double.parseDouble(txtCurrencyInput.getText());
+                lblAmountInputDisplay.setText(Double.toString(currency));
+
+                int cents = (int) (currency * 100);
+
+                int toonies = cents / 200;
+                cents %= 200;
+                lblTooniesOutput.setText(Integer.toString(toonies));
+
+                int loonies = cents / 100;
+                cents %= 100;
+                lblLooniesOutput.setText(Integer.toString(loonies));
+
+                int quarters = cents / 25;
+                cents %= 25;
+                lblQuartersOutput.setText(Integer.toString(quarters));
+
+                int dimes = cents / 10;
+                cents %= 10;
+                lblDimesOutput.setText(Integer.toString(dimes));
+
+                int nickels = cents / 5;
+                cents %= 5;
+                lblNickelsOutput.setText(Integer.toString(nickels));
+
+                int pennies = cents;
+                lblPenniesOutput.setText(Integer.toString(cents));
             }
         });
     }
@@ -29,4 +70,5 @@ public class changeExchange extends JFrame {
     public static void main(String[] args) {
         new changeExchange();
     }
+
 }
